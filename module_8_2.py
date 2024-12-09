8d8f922f93e4c2286ec7953d7abb6edf7ba2a11b
recrutor25 <sommersi@yandex.ru>
2024-12-09 21:33:57 +0200

    09.12.24

--- module_8_2.py
+++ module_8_2.py
@@ -1,6 +1,5 @@
 #!/home/igor/PycharmProjects/module_8/.venv/bin/python
 # -*- coding: utf-8 -*-
-
 def personal_sum(numbers):
     result = 0
     incorrect_data = 0
@@ -27,7 +26,6 @@ def calculate_average(numbers):
         return 0
 
     return average
-
 print(f'Результат 1: {calculate_average("1, 2, 3")}')  # Строка перебирается, но каждый символ - строковый тип
 print(f'Результат 2: {calculate_average([1, "Строка", 3, "Ещё Строка"])}')  # Учитываются только 1 и 3
 print(f'Результат 3: {calculate_average(567)}')  # Передана не коллекция
